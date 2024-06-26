0ed5f15cb07d93fe759adecf0168445ef1943e2f
Alexpc18 <alexanderpc18@hotmail.com>
2024-06-26 06:59:23 +0000

    feat script 0 to 2

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,11 @@
+// Using Process stdin
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const text = process.stdin.read();
+  if (text){
+    process.stdout.write(`Your name is: ${text}`);
+    process.stdout.write('This important software is now closing\n');
+  }
+});
